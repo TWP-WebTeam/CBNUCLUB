a5cb23fdb5dc86e468e4d5cd8117d01f1a77f5ab
Lee-Ha-Eun-00 <lhe543@naver.com>
2021-06-13 19:17:18 +0900

    QNA수정

--- php/community_QNA_write.php
+++ php/community_QNA_write.php
@@ -9,7 +9,7 @@ include "../include/dbConnect.php";
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="community_QNA_write.css">
+      <link rel="stylesheet" href="../css/community_QNA_write.css">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
       <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
       <meta name="viewport" content="width=device-width, initial-scale=1">
